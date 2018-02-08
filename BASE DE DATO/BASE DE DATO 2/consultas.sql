07ee6817803bdd696d88dd161fb3c61cdc2e77c7
Juanbernaortega <36245670+Juanbernaortega@users.noreply.github.com>
2018-02-08 14:45:06 -0500

    Add files via upload

---
+++ BASE DE DATO/BASE DE DATO 2/consultas.sql
@@ -0,0 +1,8 @@
+
+Select nombre, apellido from cliente where fecha_ingre=”01/01/2018” between “32/12/2018”
+Update REPORTE_FIN from REPORTE where id_cliente = (select id_cliente from  cliente_maquina where id_maquina =(select id_maquina from maquina where id_maquina = 1)
+Delete * FROM maquina where FECHA_INGRE = 20/12/2019
+Insert into maquina (nombre, marca, repor_ingre) value (OMEN, hp, dano de hardware) where cliente.nombre = “JUAN”
+
+
+
